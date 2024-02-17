1d82285629d2ce61c3034f182af3f8a1c53eeee3
Lucas Rocha Fernandes <lucasrochaf2018@gmail.com>
2024-02-17 01:43:32 -0300

    add envSchema with config service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,18 @@ import { Module } from '@nestjs/common'
 
 import { PrismaService } from './prisma/prisma.service'
 import { CreateAccountController } from './controllers/users/create-account.controller'
+import { ConfigModule } from '@nestjs/config'
+import { envSchema } from './env'
 
 @Module({
-  imports: [],
+  imports: [
+    // Module de configuração para variáveis ambiente
+    // forRoot é usado para configurar módulos
+    ConfigModule.forRoot({
+      validate: (env) => envSchema.parse(env),
+      isGlobal: true,
+    }),
+  ],
   controllers: [CreateAccountController],
   providers: [PrismaService],
 })
